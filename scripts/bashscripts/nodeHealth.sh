dce2f062f7bf81dada8ffe84b5ffc655f914b48a
junman99 <chinjunman99@gmail.com>
2024-03-23 16:28:08 +0800

    commit

--- scripts/bashscripts/nodeHealth.sh
+++ scripts/bashscripts/nodeHealth.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 ########################################
 # Author: Jun Man
 # Date: 23/03/2023
@@ -8,6 +9,8 @@
 ########################################
 
 set -x #debug mode
+set -e #exit the script when there is an error
+set -o pipefail #to exit when there is a pipefail 
 
 echo "Print the disk space"
 df -h 
@@ -17,3 +20,6 @@ free -g
 
 echo "Print the CPU"
 nproc 
+
+echo "Print PID of amazon (at the second column)"
+ps -ef | grep amazon | awk -F" " '{print $2}'
