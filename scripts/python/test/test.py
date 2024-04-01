915defab493aef69c9dc09b3b427b59dbef00e7c
junman99 <chinjunman99@gmail.com>
2024-04-01 20:53:25 +0800

    adding python scripts

--- scripts/python/test/test.py
+++ scripts/python/test/test.py
@@ -1,4 +1,32 @@
-x = 10000000000
+#### delimit thousands ####
+def format_num(num):
+    fmt_num = {:,}.format(num)
+    return fmt_num
 
-amend = "{:,}".format(x)
-print (amend)
+#### sum of 1-10 ####
+def sum_of_num(range):
+    sum = 0
+    for i in range(1,10):
+        sum += i
+    return sum 
+
+#### flip a string ####
+def flip_line(words):
+    x = "hello how are you"
+    fmt_line = x[::-1]
+    return fmt_line
+
+#### fib seq ####
+def fib_seq(n):
+    sequence=[0,1]
+    while len(sequence) < n:
+        next_term = sequence[-1] + sequence[-2]
+        sequence.append(next_term)
+    return sequence
+
+#### angle btw circles ####
+import math
+
+def angle(arc_length, radius):
+    angle = (arc_lengh/radius)*(180/math.pi)
+    return angle
